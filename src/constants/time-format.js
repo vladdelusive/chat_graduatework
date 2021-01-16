b15e47363f6d47f855c64b1302dc70a331f6089a
vladdelusive <mr.tovstochub@mail.ru>
2021-01-16 20:49:29 +0200

    add: timestamp format time (moment js

---
+++ src/constants/time-format.js
@@ -0,0 +1,6 @@
+export const CLIENT_DATE_TIME_FORMAT = 'DD.MM.YYYY HH:mm:ss';
+export const CLIENT_DATE_FORMAT = 'DD.MM.YYYY';
+export const CLIENT_TIME_FORMAT = 'HH:mm:ss';
+export const CLIENT_DAY_MONTH_FORMAT = 'DD.MM';
+export const CLIENT_DATE_HH_MM = 'DD.MM.YYYY HH:mm';
+export const HH_MM = 'HH:mm';
