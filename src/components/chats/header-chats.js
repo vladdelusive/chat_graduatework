89b02d178caf819aebdb23870a02370e92597fbf
vladdelusive <mr.tovstochub@mail.ru>
2021-05-10 14:23:53 +0300

    feat: lastTime rerender for recalculation last time online with moment()

--- src/components/chats/header-chats.js
+++ src/components/chats/header-chats.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { Button, Col, Input, Row, Tooltip, Typography, Avatar } from 'antd';
+import { Button, Input, Tooltip, Typography, Avatar } from 'antd';
 import {
     PlusOutlined, UserOutlined,
 } from '@ant-design/icons';
@@ -29,6 +29,19 @@ function HeaderChats(props) {
     const [isShowNewChatModal, setIsShowNewChatModal] = useState(false)
 
     const onlineStatus = activeChat?.userInfo?.status;
+
+    const [, setRerender] = useState()
+
+    useEffect(() => {
+        let interval;
+        if (onlineStatus?.lastTime) {
+            interval = setInterval(() => {
+                setRerender(Date.now())
+            }, 60000);
+        }
+        return () => clearInterval(interval);
+        // eslint-disable-next-line
+    }, [onlineStatus?.lastTime])
 
     return (
         <React.Fragment>
@@ -97,65 +110,6 @@ function HeaderChats(props) {
                             {isSetActiveChat ? <VideoCallButton profile={activeChat.userInfo} /> : null}
                         </div>
                     </div>
-                    {/* <Row typeof="flex" justify="space-between" align="middle" style={{ height: "100%", padding: "0px 10px" }}>
-                        <Col span={20}>
-                            <Row align="middle" gutter={10}>
-                                <Col>
-                                    <Avatar
-                                        src={isSetActiveChat && activeChat?.userInfo?.photo ? activeChat.userInfo.photo : profile?.photo}
-                                        icon={<UserOutlined />}
-                                        size="large"
-                                        style={{
-                                            color: "white",
-                                            background: "#5d86e8",
-                                            fontSize: 40, width: 60,
-                                            height: 60, lineHeight: "55px"
-                                        }}
-                                    />
-                                </Col>
-                                <Col>
-                                <div>
-                                    
-                                </div>
-                                    <Row>
-                                        <Title level={3}>
-                                            {isSetActiveChat && activeChat?.userInfo?.name ?
-                                                <Link to={routes["profiles"].link(activeChat.userInfo.uid)} style={{ color: "#2335a0" }}>
-                                                    {activeChat.userInfo.name}
-                                                </Link>
-                                                : profile?.name || "Владислав Товсточуб"}
-                                        </Title>
-                                    </Row>
-                                    <Row style={{
-                                        position: "absolute",
-                                        top: 35,
-                                        width: "100%"
-                                    }}>
-                                        {
-                                            onlineStatus
-                                                ? onlineStatus?.online
-                                                    ?
-                                                    <div className="status status-online">
-                                                        <div className="status__text status-online__text">В мережі</div>
-                                                        <div className="status__icon status-online__icon"></div>
-                                                    </div>
-                                                    :
-                                                    <div className="status status-offline">
-                                                        <div className="status__text status-offline__text">Був в мережі {moment(+onlineStatus.lastTime).fromNow()}</div>
-                                                        <div className="status__icon status-offline__icon"></div>
-                                                    </div>
-                                                : null
-                                        }
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Col>
-                        {isSetActiveChat ? <Col>
-                            <Row>
-                                <VideoCallButton profile={activeChat.userInfo} />
-                            </Row>
-                        </Col> : null}
-                    </Row> */}
                 </div>
             </div>
             { isShowNewChatModal ? <SearchChatModal setIsShowNewChatModal={setIsShowNewChatModal} /> : null}
